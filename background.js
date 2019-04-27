d43c445c941d4ed5ac1b868827df3bc77eabffc1
Thom Bilton <thombilton@gmail.com>
2019-04-27 15:06:24 +1200

    added basic ui and can make CROG request to backend

--- background.js
+++ background.js
@@ -1,14 +1,41 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({
+    color: '#3aa757'
+  }, function () {
+    console.log("The color is green.");
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: '.aucklandcouncil.govt.nz'},
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {
+        hostContains: '.aucklandcouncil.govt.nz'
+      },
+    })],
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+
+    let responseJson
+
+    if (request.contentScriptQuery == 'goButton') {
+      fetch('http://aklc-help-server.herokuapp.com/gather/byid/2874913')
+      .then(function(data){
+        return data.json()
       })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+      .then(function(myJson){
+        consoleLog(JSON.stringify(myJson))
+        sendResponse(myJson)
+      })
+      return true
+    }
+  }
+)
+
+consoleLog = function (input) {
+  chrome.extension.getBackgroundPage().console.log(input);
+}
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let changeColor = document.getElementById('changeColor');
+let goButton = document.getElementById('goButton');
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -13,3 +14,21 @@ changeColor.onclick = function(element) {
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
   };
+
+goButton.onclick = function(element) {
+  let url = 'http://aklc-help-server.herokuapp.com/gather/byid/2874913'
+
+  chrome.extension.getBackgroundPage().console.log('foo');
+  chrome.runtime.sendMessage({ contentScriptQuery : 'goButton', url: this.url}, function(response){
+    consoleLog(response);
+  })
+/*   fetch('http://aklc-help-server.herokuapp.com/gather/byid/2874913')
+  .then(function(response){
+      consoleLog(response.json());
+  }) */
+}
+
+
+consoleLog = function(input){
+  chrome.extension.getBackgroundPage().console.log(input);
+}
